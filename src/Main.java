39a62ab6dbd52fb2bf33855babab5c50e166f438
Your Name <andjelkoa@gmail.com>
2023-01-06 11:09:51 +0100

    final version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String [] predmet = {"bicikl", "automobil", "telefon"};
+        String [] imePredmeta = {"BMX", "Audi", "Samsung"};
+        double [] cena = {215.54, 1000.00, 400.44};
+
+        for (int i = 0; i < predmet.length; i++) {
+            System.out.println(imePredmeta[i] + " je " + predmet[i] + ", i kosta " + cena[i] + "eur-a.");
+        }
     }
 }
